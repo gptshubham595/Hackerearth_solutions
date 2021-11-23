c1a33499b5b6b4539ea148a86ce30f2f099607d6
Shubham gupta Ggps <24877361+gptshubham595@users.noreply.github.com>
2021-11-24 02:05:42 +0530

    Update prime range.cpp

--- prime range.cpp
+++ prime range.cpp
@@ -1,3 +1,10 @@
+you are given [L,R] you are required to determine the prime rangers in this range.The definition of prime rangers is that frequency of the smallest prime digit must be greater than or equal to the frequency of the second smallest prime digit in a number the frequency of the second smallest prime digit is greater than or equal to the frequency of the third smallest prime digit in a number and so on
+Note:
+the number of prime rangers may become very large therefore print the prime rangerd modulo 10^9+7
+inputformat:
+the first line contains an integer T denoting the number of test cases
+next T lines contains two spaced seperated integer L and R denoting the range in which you have to find the prime rangers
+
 int main()
 
 {
